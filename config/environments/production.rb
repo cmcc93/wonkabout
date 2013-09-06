64405c850312343de2ec4a7c1d47e1824562643b
Casey McCourt <cas.mcco@gmail.com>
2013-09-06 15:00:42 -0700

    more-config-file-changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-WonkInfo::Application.configure do
+WonkAboutDev::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
