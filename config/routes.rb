ef5656c4c66994187c38d348dd30b2bfa5a40029
Casey McCourt <cas.mcco@gmail.com>
2013-09-09 13:26:38 -0700

    configuration and deleting of public folder

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 WonkAboutDev::Application.routes.draw do
   
-  root :to => 'public#about'
-  match 'about', :to => 'public#about'
+  root :to => "public#about"
+  match "about", :to => "public#about"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
